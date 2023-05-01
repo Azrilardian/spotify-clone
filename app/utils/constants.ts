d89418be923eba255dc58fe64587118ceb8d3223
Azril Ardian Ifansyah <irfan05ariell@gmail.com>
2023-05-01 22:33:18 +0800

    feat(folder): create utils folder include constants js file inside constants js file, include music genres and links constants variable

---
+++ app/utils/constants.ts
@@ -0,0 +1,55 @@
+const MUSIC_GENRES = [
+  {
+    title: 'Pop',
+    value: 'POP',
+  },
+  {
+    title: 'Rock',
+    value: 'ROCK',
+  },
+  {
+    title: 'K-Pop',
+    value: 'K_POP',
+  },
+  {
+    title: 'Dance',
+    value: 'DANCE',
+  },
+  {
+    title: 'Electronic',
+    value: 'ELECTRONIC',
+  },
+  {
+    title: 'Pop',
+    value: 'POP',
+  },
+  {
+    title: 'Pop',
+    value: 'POP',
+  },
+]
+
+const LINKS = [
+  {
+    name: 'Discover',
+    to: '/',
+    icon: '',
+  },
+  {
+    name: 'Around You',
+    to: '/around-you',
+    icon: '',
+  },
+  {
+    name: 'Top Artists',
+    to: '/top-artits',
+    icon: '',
+  },
+  {
+    name: 'Top Charts',
+    to: '/top-charts',
+    icon: '',
+  },
+]
+
+export { MUSIC_GENRES, LINKS }
