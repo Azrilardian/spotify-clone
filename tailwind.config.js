8382d9edc1bd118fa0eb49a1ef1c287046368c47
Azril Ardian Ifansyah <irfan05ariell@gmail.com>
2023-05-10 16:48:59 +0800

    feat(app): adding color to tailwind config and creating fonts folder adding some base spotify base color to tailwind config and creating fo

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,16 @@ module.exports = {
     './app/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
+    colors: {
+      mainGreen: '#1db954',
+      mainBlack: '#121212',
+      mainGray: '#b3b3b3',
+      mainDarkGray: '#535353',
+      mainDarkestGray: '#212121',
+    },
+    fontFamily: {
+      primary: ['Gotham', 'sans-serif'],
+    },
     extend: {},
   },
   plugins: [],
