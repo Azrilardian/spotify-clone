c8618aaf40227e8f24e71478c2b945fd32357a39
Azril Ardian Ifansyah <irfan05ariell@gmail.com>
2023-05-18 11:48:27 +0800

    style(typography): adding h1, h2, and h3 base style adding h1, h2, and h3 base style to typography css BREAKING CHANGE: adding padding to main layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,7 +15,9 @@ export default function RootLayout({
     <html lang="en">
       <body className="flex">
         <Sidebar />
-        <main className="basis-10/12 h-screen bg-main-green">{children}</main>
+        <main className="basis-10/12 h-screen bg-main-green px-10 pt-14 pb-10">
+          {children}
+        </main>
       </body>
     </html>
   )
--- app/page.tsx
+++ app/page.tsx
@@ -5,6 +5,9 @@ import Discover from '@/components/discover/discover'
 const Dashboard = () => {
   return (
     <>
+      <h1 className="h1">Kopikustik</h1>
+      <h2 className="h2">Selamat Pagi</h2>
+      <h3 className="h3">Dibuat Untuk Azril Ardian Ifansyah</h3>
       <Discover />
     </>
   )
